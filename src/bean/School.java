83e33e19591f0f12ec54300cb9fd0505993f882b
n_ono <n_ono@LAPTOP-5291B3OT>
2025-06-04 13:40:19 +0900

    a

--- src/bean/School.java
+++ src/bean/School.java
@@ -1,5 +1,30 @@
 package bean;
 
 public class School {
+	private String cd;
+	private String name;
+	
+	//デフォルトコンストラクタ
+	public School() {
+		
+	}
+	
+	//cd(学校コード)
+	public String getCd() {
+		return cd;
+	}
+	
+	public void setCd(String cd) {
+		this.cd = cd;
+	}
+	
+	//name(学校名)
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
--- src/bean/Student.java
+++ src/bean/Student.java
@@ -1,5 +1,65 @@
 package bean;
 
 public class Student {
+	private String no;
+	private String name;
+	private String entYear;
+	private boolean isAttend;
+	private String classNum;
+	private School school;
+	
+	public Student() {
+		
+	}
+	
+	public String getNo() {
+		return no;
+	}
+	
+	public void setNo(String no) {
+		this.no = no;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEntYear() {
+		return entYear;
+	}
+	
+	public void setEntYear(String entYear) {
+		this.entYear = entYear;
+	}
+	
+	public boolean isAttend() {
+		return  isAttend;
+	}
+	
+	public void setAttend(boolean isAttend) {
+		this.isAttend = isAttend;
+	}
+	
+	public String getClassNum() {
+		return classNum;
+	}
+	
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+	
+	public School getSchool() {
+		return school;
+	}
+	
+	public void setSchool(School School) {
+		this.school = school;
+	}
 
 }
+
+
